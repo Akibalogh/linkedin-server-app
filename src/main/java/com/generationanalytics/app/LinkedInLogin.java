556eff5ab37591664a1c0de4d940b8ba107beba5
Aki Balogh <akibalogh@gmail.com>
2013-01-25 15:18:09 -0600

    still debugging 400 error.. working in example file..

--- src/main/java/com/generationanalytics/app/LinkedInLogin.java
+++ src/main/java/com/generationanalytics/app/LinkedInLogin.java
@@ -4,8 +4,6 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import java.util.Properties;
-
 import com.google.code.linkedinapi.schema.Person;
 import com.google.code.linkedinapi.client.oauth.*;
 import com.google.code.linkedinapi.client.LinkedInApiClient;
@@ -14,8 +12,8 @@ import com.google.code.linkedinapi.client.LinkedInApiClientFactory;
 @SuppressWarnings("serial")
 public class LinkedInLogin extends HttpServlet {
 
-	private static final String CONSUMER_KEY="lffo6yr4wx9t";
-	private static final String CONSUMER_SECRET="ns8Br5H7hREyRCQq";
+	private static final String CONSUMER_KEY="34eiwzgyjlyn";
+	private static final String CONSUMER_SECRET="Oo0P0QU0oBaIYs9W";
 
 	private static String tokenValue;
 	private static String tokenSecret;
@@ -25,9 +23,6 @@ public class LinkedInLogin extends HttpServlet {
     throws IOException, ServletException
     {
 	String callbackUrl = "http://50.22.200.186:8080/servlet/LinkedInLogin";
-	Properties systemSettings = System.getProperties();
-	// systemSettings.put("http.proxyHost", "proxy");
-	// systemSettings.put("http.proxyPort", "8080");
 	response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 	out.println("<html>");
@@ -35,8 +30,12 @@ public class LinkedInLogin extends HttpServlet {
 
 	final LinkedInOAuthService oauthService = LinkedInOAuthServiceFactory.getInstance().createLinkedInOAuthService(CONSUMER_KEY, CONSUMER_SECRET);
 
-	LinkedInRequestToken requestToken = oauthService.getOAuthRequestToken(callbackUrl);
-	// LinkedInRequestToken requestToken = oauthService.getOAuthRequestToken();
+	out.println("<br>Request Token: " + request.getParameter("requestToken")+ "<br>");
+
+	LinkedInRequestToken requestToken = (LinkedInRequestToken) request.getAttribute("requestToken");
+
+	if (requestToken == null)
+	{ requestToken = oauthService.getOAuthRequestToken(callbackUrl); }
 
 	String oauthVerifier = request.getParameter("oauth_verifier");
 	
@@ -53,19 +52,27 @@ public class LinkedInLogin extends HttpServlet {
 	out.println("<br>token secret: " + requestToken.getTokenSecret());
 	out.println("<br>expiration time: " + requestToken.getExpirationTime());
 
-	LinkedInAccessToken accessToken = oauthService.getOAuthAccessToken(requestToken, oauthVerifier);
+	out.println("<br>parameter map: " + request.getParameterMap());
+	out.println("<br>oauth token: " + request.getParameter("oauth_token"));
+	out.println("<br>oauth verifier: " + request.getParameter("oauth_verifier"));
 
+	out.println("<br>header: " + request.getHeaders("ValuesEnumerator").hasMoreElements());
+
+
+	// TODO: Returns 400 error
+	LinkedInAccessToken accessToken = oauthService.getOAuthAccessToken(requestToken, oauthVerifier);
 /*
 	final LinkedInApiClientFactory factory = LinkedInApiClientFactory.newInstance(CONSUMER_KEY, CONSUMER_SECRET);
+
 	final LinkedInApiClient client = factory.createLinkedInApiClient(accessToken);
 
+	out.println("<br><br>access token: " + request.getParameter("accessToken"));
 	Person profile = client.getProfileForCurrentUser();
 
 	out.println("Fetching profile for current user");
 	out.println("Headline: " + profile.getHeadline());
-*/
     	out.println("</body>");
         out.println("</html>");
-    
+  */  
     }
 }
