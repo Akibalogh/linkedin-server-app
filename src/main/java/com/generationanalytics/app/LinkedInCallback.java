126b9e4e397bc58aeffd55505d69535689dcef49
Aki Balogh <akibalogh@gmail.com>
2013-01-30 12:42:19 -0600

    saving linkedin profile to disk

--- src/main/java/com/generationanalytics/app/LinkedInCallback.java
+++ src/main/java/com/generationanalytics/app/LinkedInCallback.java
@@ -4,6 +4,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import java.util.Date;
 import java.util.Enumeration;
 
 import org.scribe.builder.ServiceBuilder;
@@ -17,14 +18,17 @@ public class LinkedInCallback extends HttpServlet {
 	private static final String CONSUMER_KEY="34eiwzgyjlyn";
 	private static final String CONSUMER_SECRET="Oo0P0QU0oBaIYs9W";
 	private static String PROTECTED_RESOURCE_URL = "http://api.linkedin.com/v1/people/~:(id,first-name,last-name,industry,summary,specialties,positions,email-address,associations,honors,interests,publications,skills,twitter-accounts)";
+	private static String profile;
+
+	private static final String OUTPATH = "/usr/share/downloaded-li-profiles";
+	private static String outFileName;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
     {
 	response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-	out.println("<html>");
-        out.println("<body>");
+	profile += "<html>\n";
+        profile += "<body>\n";
 
 	OAuthService service = new ServiceBuilder()
 		.provider(LinkedInApi.class)
@@ -41,11 +45,29 @@ public class LinkedInCallback extends HttpServlet {
 	Token accessToken = service.getAccessToken(requestToken, verifier);
 
 	OAuthRequest apiRequest = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+	// Set a header to tell the server to return JSON
+	apiRequest.addHeader("x-li-format", "json");
 	service.signRequest(accessToken, apiRequest);
 	Response apiResponse = apiRequest.send();
 
-    	out.println(apiResponse.getBody());
-	out.println("</body>");
-        out.println("</html>");
-    }
+	profile += apiResponse.getBody() + "\n";
+	profile += "</body>\n";
+        profile += "</html>";
+       
+	// Write the API response to the user
+	PrintWriter outHTML = response.getWriter();
+	outHTML.println("Thanks! We'll be sending you emails going forward.");
+
+	// The timestamp will be used to name the file
+	java.util.Date date = new java.util.Date();
+	outFileName = String.valueOf(date.getTime());
+	
+	// Write the API response to a file
+	File outFile = new File(OUTPATH + "/" + outFileName);
+	FileWriter fstream = new FileWriter(outFile);
+	BufferedWriter fout = new BufferedWriter(fstream);
+
+	fout.write(profile);
+	fout.close();
+	}
 }
